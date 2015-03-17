6143fa1ec1c289531ab34d971fa294e1ab87ef06
Eyana <eyana.mallari@obf.ateneo.edu>
2015-03-18 00:17:39 +0800

    reset votes, frontend tweaks. up next: eligibility (called or not called)

--- application/views/admin/menu/candidates.php
+++ application/views/admin/menu/candidates.php
@@ -1,5 +1,8 @@
 <div class="page-nav">
-	
+
+	<a class="btn btn-primary btn-sm" href="<?php echo site_url('admin/votes/create'); ?>">
+		 Cast Votes
+	</a>
 	<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#resetvotes">
 		 Reset Votes
 	</button>
