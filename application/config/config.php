31acc4266a67f77ae54d09ecea4f70c7b235d96a
Adrian <adrian.tiu@obf.ateneo.edu>
2018-05-21 10:32:56 +0800

    replace ip url with localhost + remove db root password

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.0.108/aclc_comelec/';
+$config['base_url']	= 'http://localhost/aclc_comelec/';
 
 /*
 |--------------------------------------------------------------------------
